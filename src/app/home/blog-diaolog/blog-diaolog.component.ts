2c5401b04d07af66480903c2c411791457490046
Ersin Aydın <ersinaydinworks@gmail.com>
2023-01-11 17:31:22 +0300

    comment listing

--- src/app/home/blog-diaolog/blog-diaolog.component.ts
+++ src/app/home/blog-diaolog/blog-diaolog.component.ts
@@ -1,16 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-blog-diaolog',
   templateUrl: './blog-diaolog.component.html',
   styleUrls: ['./blog-diaolog.component.scss']
 })
-export class BlogDiaologComponent {
+export class BlogDiaologComponent implements OnInit {
   isUpdate: boolean = false;
   imageUrl: string = "";
   title: string = "";
   body: string = "";
+  commentData: any;
+  private commentService = inject(CommentService);
   private data: any = inject(MAT_DIALOG_DATA);
   private dialogRef = inject(MatDialogRef<BlogDiaologComponent>)
   constructor() {
@@ -22,6 +25,11 @@ export class BlogDiaologComponent {
       this.title = this.data.blog.title;
       this.body = this.data.blog.body;
     }
+  }
+  ngOnInit(): void {
+    this.commentService.getComments().subscribe((res) => {
+      this.commentData = res.filter((x: { postId: any }) => x.postId == this.data.blog.id)
+    })
   }
   close() {
     this.dialogRef.close();
